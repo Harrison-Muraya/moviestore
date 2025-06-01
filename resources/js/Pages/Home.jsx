1343a14f68074d139ae61f671db6ff0b170b1cbd
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-06-01 03:40:41 +0300

    Update Home component with new h1 element styles

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home(){
     return (
         <>
-        <h1> welcome Maina</h1>
+        <h1 className="flex text-gray-800 border-2 border-teal-800 items-center justify-center mx-12 my-8 p-6 bg-teal-100" > welcome Maina</h1>
         </>
     )
 }
