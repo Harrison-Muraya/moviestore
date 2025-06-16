ea664407005686d3917624752c1a965b54ed44fd
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-06-16 22:59:46 +0300

    added edit file

---
+++ resources/js/Pages/VideoEdit.jsx
@@ -0,0 +1,12 @@
+
+
+function VideoEdit(){
+    return (
+        <div>
+            <h1>Edit Video</h1>
+            <p>This is the video edit page.</p>
+        </div>
+    );
+}
+
+export default VideoEdit;
