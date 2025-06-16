81394d71a508e7f55046036447d9c4101d45be42
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-06-16 23:14:19 +0300

    Added new routes for movie management in ContentUploadController

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,11 @@ Route::get('/api/genres', function () {
 Route::controller(ContentUploadController::class)->group(function(){
     Route::get('/upload-movies', 'index')->name('storeview');   
     Route::post('/upload-content', 'store')->name('uploadContent');
+
+    Route::get('/movie-list', 'movieList')->name('movieList');
+    Route::post('/update-movie/{id}', 'update')->name('updateMovie');
+    Route::get('/movie-details/{id}', 'show')->name('movieDetails');    
+    Route::get('/delete-movie/{id}', 'destroy')->name('deleteMovie');
 });
 
 Route::middleware('auth')->group(function () {
