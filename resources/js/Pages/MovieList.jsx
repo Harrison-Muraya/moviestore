6a4f2a2985f192e2607f6c7177c41158db108566
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-06-17 22:48:51 +0300

    Removed empty lines and commented out code in MovieList.jsx

--- resources/js/Pages/MovieList.jsx
+++ resources/js/Pages/MovieList.jsx
@@ -13,9 +13,9 @@ function MovieList() {
             <div className="relative bg-[url('/images/silence.jpg')]  h-screen bg-cover bg-center bg-no-repeat">
                 <div className="absolute inset-0 bg-white bg-opacity-20 z-20 backdrop-blur-sm"></div>
                 <div className="relative container bg-red-50 z-30 mx-auto">
-                    
+
                 </div>
-                
+
             </div>
         </>
 
