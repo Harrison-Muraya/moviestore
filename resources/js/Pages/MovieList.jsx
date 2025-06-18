3302164fd503d702723b2eedfbf98ffd2c3cfcb7
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-06-18 11:25:48 +0300

    Refactor MovieList component layout and add new elements

--- resources/js/Pages/MovieList.jsx
+++ resources/js/Pages/MovieList.jsx
@@ -12,7 +12,13 @@ function MovieList() {
         <>
             <div className="relative bg-[url('/images/silence.jpg')]  h-screen bg-cover bg-center bg-no-repeat">
                 <div className="absolute inset-0 bg-white bg-opacity-20 z-20 backdrop-blur-sm"></div>
-                <div className="relative container bg-red-50 z-30 mx-auto">
+                <div className="relative container grid grid-flow-row z-30 mx-auto">
+                    <div className="bg-white/20 backdrop-blur-sm p-2 md:p-2 rounded-lg shadow-lg">
+                        <p className="text-indigo-600 text-2xl md:text-4xl my-6 text-center m font-semibold tracking-wider">
+                            ALPHA <span className="text-red-600">.</span>
+                        </p>
+                    </div>
+                    <div className="text-center bg-red-300 mt-4">hello</div>
 
                 </div>
 
