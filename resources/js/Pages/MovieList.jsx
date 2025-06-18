80fa00d8aa05204ba44241d0e6116a17f3668b48
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-06-18 23:48:39 +0300

    Refactor MovieList component: update background, add image thumbnails, and modify layout

--- resources/js/Pages/MovieList.jsx
+++ resources/js/Pages/MovieList.jsx
@@ -8,14 +8,14 @@ function MovieList() {
     const movies = props.movies || [];
     const user = props.testing || []
 
-    // console.log("user", user);
+    console.log("Movies:", movies);
 
 
     return (
         <>
-            <div className="relative bg-[url('/images/silence.jpg')]  h-screen bg-cover bg-center bg-no-repeat">
+            <div className="fixed inset-0 overflow-hidden bg-[url('/images/silence.jpg')]  h-screen bg-cover bg-center bg-no-repeat">
                 <div className="absolute inset-0 bg-white bg-opacity-20 z-20 backdrop-blur-sm"></div>
-                <div className="relative container grid grid-flow-row z-30 mx-auto">
+                <div className="relative container grid grid-flow-row z-30 mx-auto overflow-auto h-screen">
                     <div className="bg-white/20 backdrop-blur-sm p-2 md:p-2 rounded-lg shadow-lg">
                         <p className="text-indigo-600 text-2xl md:text-4xl my-6 text-center m font-semibold tracking-wider">
                             ALPHA <span className="text-red-600">.</span>
@@ -30,7 +30,22 @@ function MovieList() {
                                 <ul>
                                     {movies.map((movie, index) => (
                                         <li key={index} className="text-white text-lg bg-black/40 backdrop-blur-sm md:text-2xl p-2 my-6 font-semibold tracking-wider rounded-sm">
-                                            {movie.title}
+                                            <div className="grid grid-cols-2">
+                                                <div className="w-16 h-16 md:w-40 md:h-40">
+                                                    <img src={movie.thumbnail} alt={movie.title} className="w-full h-full object-cover rounded-md" />
+                                                </div>
+                                                <div className="flex items-center justify-center">
+                                                    <span className="text-red-600 text-lg md:text-2xl font-bold">{movie.title}</span>
+                                                </div>
+                                            </div>
+                                            <div className="grid grid-cols-2">
+                                                <div className="w-16 h-16 md:w-40 md:h-40">
+                                                    <img src={movie.thumbnail} alt={movie.title} className="w-full h-full object-cover rounded-md" />
+                                                </div>
+                                                <div className="flex items-center justify-center">
+                                                    <span className="text-red-600 text-lg md:text-2xl font-bold">{movie.title}</span>
+                                                </div>
+                                            </div>
                                         </li>
                                     ))}
                                 </ul>
