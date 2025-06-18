4433b9967fd6759619c12f1e105d8a17b28016b4
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-06-19 02:47:03 +0300

    added series section logig

--- resources/js/Pages/MovieList.jsx
+++ resources/js/Pages/MovieList.jsx
@@ -5,10 +5,14 @@ function MovieList() {
 
     const { props } = usePage();
     const [singleMovies, setSingleMovies] = useState([]);
+    const [SeriesMovies, setSeriesMovies] = useState([]);
+
     const movies = props.movies || [];
-    
+
     useEffect(() => {
         const singles = movies.filter(movie => movie.type === 'movie');
+        const series = movies.filter(movie => movie.type === 'series');
+        setSeriesMovies(series);
         setSingleMovies(singles);
     }, [movies]);
 
@@ -25,55 +29,101 @@ function MovieList() {
                 </div>
 
                 <div className="grid grid-cols-2 mt-6 gap-x-5">
-                    <div className="grid grid-row bg-white/20 backdrop-blur-sm p-2 rounded-lg shadow-lg">
+                    {/* Single Movies Section */}
+                    <div className="bg-white/20 backdrop-blur-sm p-2 rounded-lg shadow-lg">
                         <p className="text-white text-xl md:text-4xl my-6 font-semibold tracking-wider">
                             Single Movies
                         </p>
 
                         <div className="card">
                             <ul>
-                                {/* if (movies.type === 'movie') {
-                                    {
-                                        movies.map((movie, index) => (
-                                            <li
-                                                key={index}
-                                                className="text-white text-lg bg-white/60 backdrop-blur-sm md:text-2xl p-2 my-6 font-semibold tracking-wider rounded-sm"
-                                            >
-                                                <div className="flex flex-col md:flex-row">
-                                                    <div className="h-16 md:w-40 md:h-40">
-                                                        <img
-                                                            src={movie.thumbnail}
-                                                            alt={movie.title}
-                                                            className="w-full h-full object-cover rounded-md"
-                                                        />
-                                                    </div>
-                                                    <div className="justify-left ml-4">
-                                                        <div className="flex flex-row ">
-                                                            <span className="text-black text-lg md:text-2xl font-bold capitalize">
-                                                                {movie.title}
-                                                            </span>
-                                                            <span className="bg-indigo-600 text-gray-100 text-sm  ml-2 rounded-sm px-2 py-1">
-                                                                {movie.genres[0]?.name || 'Unknown Genre'}
-                                                            </span>
-                                                        </div>
-                                                        <div className="">Description</div>
 
+                                {singleMovies.length > 0 ? (
+                                    singleMovies.map((movie, index) => (
+                                        <li
+                                            key={movie.id || index}
+                                            className="text-white text-lg bg-white/60 backdrop-blur-sm md:text-2xl p-2 my-6 font-semibold tracking-wider rounded-sm"
+                                        >
+                                            <div className="flex flex-col md:flex-row">
+                                                <div className="h-16 md:w-40 md:h-40">
+                                                    <img
+                                                        src={movie.thumbnail}
+                                                        alt={movie.title}
+                                                        className="w-full h-full object-cover rounded-md"
+                                                    />
+                                                </div>
+                                                <div className="justify-left ml-4">
+                                                    <div className="flex flex-row ">
+                                                        <span className="text-black text-lg md:text-2xl font-bold capitalize">
+                                                            {movie.title}
+                                                        </span>
+                                                        <span className="bg-indigo-600 text-gray-100 text-sm  ml-2 rounded-sm px-2 py-1">
+                                                            {movie.genres[0]?.name || 'Unknown Genre'}
+                                                        </span>
+                                                    </div>
+                                                    <div className="text-base text-black font-mono">
+                                                        {movie.description || 'No description available.'}
                                                     </div>
                                                 </div>
-                                            </li>
-                                        ))
-                                    }
-                                }
-                                else { 
-                                    <li className="text-white text-lg md:text-2xl p-2 my-6 font-semibold tracking-wider rounded-sm">
-                                        No movies available
-                                    </li>                                 
-                                } */}
+                                            </div>
+                                        </li>
+                                    ))
+                                )
+                                    : (
+                                        <li className="text-white text-lg md:text-2xl p-2 my-6 font-semibold tracking-wider rounded-sm">
+                                            No movies available
+                                        </li>
+                                    )}
                             </ul>
                         </div>
                     </div>
 
-                    <div className="bg-white/20 backdrop-blur-sm p-2 rounded-lg shadow-lg">hello</div>
+                    {/* Series Movies Section */}
+                    <div className="bg-white/20 backdrop-blur-sm p-2 rounded-lg shadow-lg">
+                        <p className="text-white text-xl md:text-4xl my-6 font-semibold tracking-wider">
+                            Series Movies
+                        </p>
+
+                        <div className="card">
+                            <ul>
+
+                                {SeriesMovies.length > 0 ? (
+                                    SeriesMovies.map((movie, index) => (
+                                        <li
+                                            key={movie.id || index}
+                                            className="text-white text-lg bg-white/60 backdrop-blur-sm md:text-2xl p-2 my-6 font-semibold tracking-wider rounded-sm"
+                                        >
+                                            <div className="flex flex-col md:flex-row">
+                                                <div className="h-16 md:w-40 md:h-40">
+                                                    <img
+                                                        src={movie.thumbnail}
+                                                        alt={movie.title}
+                                                        className="w-full h-full object-cover rounded-md"
+                                                    />
+                                                </div>
+                                                <div className="justify-left ml-4">
+                                                    <div className="flex flex-row ">
+                                                        <span className="text-black text-lg md:text-2xl font-bold capitalize">
+                                                            {movie.title}
+                                                        </span>
+                                                        <span className="bg-indigo-600 text-gray-100 text-sm  ml-2 rounded-sm px-2 py-1">
+                                                            {movie.genres[0]?.name || 'Unknown Genre'}
+                                                        </span>
+                                                    </div>
+                                                    <div className="">Description</div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    ))
+                                )
+                                    : (
+                                        <li className="text-white text-lg md:text-2xl p-2 my-6 font-semibold tracking-wider rounded-sm">
+                                            No movies available
+                                        </li>
+                                    )}
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
