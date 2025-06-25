8c0eee0dba5faf58d11678da591b2f07572e0ee9
Harrison-Muraya <harrisonmuraya8@gmail.com>
2025-06-25 22:58:02 +0300

    Updated MovieList.jsx with color scheme changes and route updates

--- resources/js/Pages/MovieList.jsx
+++ resources/js/Pages/MovieList.jsx
@@ -23,7 +23,7 @@ function MovieList() {
             <div className="absolute inset-0 bg-white bg-opacity-20 z-20 backdrop-blur-sm"></div>
             <div className="relative grid grid-flow-row px-32  z-30 overflow-auto h-screen">
                 <div className="flex bg-white/20 backdrop-blur-sm p-2 mt-2 rounded-lg shadow-lg justify-center items-center">
-                    <Link href={route('dashboard')} className="text-indigo-600 text-2xl md:text-4xl my-6 text-center font-semibold tracking-wider">
+                    <Link href={route('dashboard')} className="text-red-600 text-2xl md:text-4xl my-6 text-center font-semibold tracking-wider">
                         ALPHA <span className="text-red-600">.</span>
                     </Link>
                 </div>
@@ -42,13 +42,13 @@ function MovieList() {
                                     singleMovies.map((movie, index) => (
                                         <li
                                             key={movie.id || index}
-                                            className="text-white text-lg bg-white/60 backdrop-blur-sm md:text-2xl p-2 my-6 font-semibold tracking-wider rounded-sm"
+                                            className="text-white text-lg bg-white/60 backdrop-blur-sm md:text-2xl p-2 my-6 font-semibold tracking-wider rounded-md"
                                         >
-                                            <Link href={route('video.player',{id:movie.id})} className="flex flex-col md:flex-row">
+                                            <Link href={route('newvideo.player', { id: movie.id })} className="flex flex-col md:flex-row">
                                                 <div className="h-16 md:w-40 md:h-40">
                                                     <img
                                                         // src={movie.thumbnail}
-                                                        src={movie.thumbnail?.startsWith('http')? movie.thumbnail : `/storage/${movie.thumbnail}`}
+                                                        src={movie.thumbnail?.startsWith('http') ? movie.thumbnail : `/storage/${movie.thumbnail}`}
                                                         alt={movie.title}
                                                         className="w-full h-full object-cover rounded-md"
                                                     />
@@ -58,7 +58,7 @@ function MovieList() {
                                                         <span className="text-black text-lg md:text-2xl font-bold capitalize">
                                                             {movie.title}
                                                         </span>
-                                                        <span className="bg-indigo-600 text-gray-100 text-sm  ml-2 rounded-sm px-2 py-1">
+                                                        <span className="bg-red-600 text-gray-100 text-sm  ml-2 rounded-md px-2 py-1">
                                                             {movie.genres[0]?.name || 'Unknown Genre'}
                                                         </span>
                                                     </div>
